a8da9dccb781f05af81d54e1896fbbfee625dfbb
ZONE7\tuandv <tuando1991995@gmail.com>
2023-02-23 11:58:54 +0700

    Revert "Add public, rename folder"
    This reverts commit 0fa6d23b905c37c476cc379bc28d7d2f92ea9b6a.

--- index.js
+++ index.js
@@ -2,19 +2,13 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
-//init middlewares
 app.use('/', express.static(path.join(__dirname, 'static')))
-app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-// init router
+
 app.get('/', (req, res, next) => {
     res.render('index');
 });
-
-
-//server
-const port = 5000
-const server =  app.listen(port, () => {
-    console.log(`Server start with port: ${port}`)
+app.listen(5000, () => {
+    console.log("Server start")
 })
