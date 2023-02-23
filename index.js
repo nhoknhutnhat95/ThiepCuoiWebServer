0fa6d23b905c37c476cc379bc28d7d2f92ea9b6a
ZONE7\tuandv <tuando1991995@gmail.com>
2023-02-23 11:56:28 +0700

    Add public, rename folder

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
+//init middlewares
 app.use('/', express.static(path.join(__dirname, 'static')))
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-
+// init router
 app.get('/', (req, res, next) => {
     res.render('index');
 });
-app.listen(5000, () => {
-    console.log("Server start")
+
+
+//server
+const port = 5000
+const server =  app.listen(port, () => {
+    console.log(`Server start with port: ${port}`)
 })
