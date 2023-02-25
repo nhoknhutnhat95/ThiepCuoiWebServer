104a8245ebd6d224ac5608f99246f756ef1fbef5
ZONE7\tuandv <tuando1991995@gmail.com>
2023-02-25 16:16:07 +0700

    Thêm Dung Long wedding, template1 chưa xong nhé thằng tó

--- index.js
+++ index.js
@@ -12,9 +12,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res, next) => {
     res.render('index');
 });
+app.get('/dung-long-wedding', (req, res, next) => {
+    res.render('DungLongWedding')
+})
+app.get('/template1', (req, res, next) => {
+    res.render('template1')
+})
 
 //Server
 const port = 5000
-const server =  app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server start with port: ${port}`)
 })
